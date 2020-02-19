a16c7035576f3a898a2a62aea07bf5d2c5105c45
Javier Aguilar <jvr.agx@gmail.com>
2020-02-19 10:11:41 -0700

    Pass test_it_can_show_patrons_by_exhibit_interest

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name_param)
     @name = name_param
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -19,6 +20,29 @@ class Museum
       end
     end
     recommended
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    patrons_by_exhibit = {}
+    @exhibits.each do | exhibit |
+      if !patrons_by_exhibit.key? exhibit
+        patrons_by_exhibit[exhibit] = []
+      end
+    end
+    @patrons.each do | patron |
+      patron.interests.each do | interest |
+        @exhibits.each do | exhibit |
+          if interest == exhibit.name
+            patrons_by_exhibit[exhibit] << patron
+          end
+        end
+      end
+    end
+    patrons_by_exhibit
   end
 
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -6,54 +6,124 @@ require './lib/museum'
 
 class MuseumTest < Minitest::Test
 
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "@imax",cost: 15})
+
+  end
+
   def test_it_exists
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    assert_instance_of Museum, dmns
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_instance_of Museum, @dmns
   end
 
 
   def test_it_has_attributes
-    dmns = Museum.new("Denver Museum of Nature and Science")
+    @dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_equal "Denver Museum of Nature and Science", dmns.name
-    assert_equal [], dmns.exhibits
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
   end
 
   def test_it_can_add_exhibits
-    dmns = Museum.new("Denver Museum of Nature and Science")
+    @dmns = Museum.new("Denver Museum of Nature and Science")
 
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "@imax",cost: 15})
 
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
 
-    expected = [gems_and_minerals, dead_sea_scrolls, imax]
+    expected = [@gems_and_minerals, @dead_sea_scrolls, @imax]
 
-    assert_equal expected, dmns.exhibits
+    assert_equal expected, @dmns.exhibits
   end
 
   def test_it_can_recommend_exhibits
-    dmns = Museum.new("Denver Museum of Nature and Science")
+    @dmns = Museum.new("Denver Museum of Nature and Science")
 
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "@imax",cost: 15})
 
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
 
-    patron_1 = Patron.new("Bob", 20)
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
+    patron1 = Patron.new("Bob", 20)
+    patron1.add_interest("Dead Sea Scrolls")
+    patron1.add_interest("Gems and Minerals")
 
-    patron_2 = Patron.new("Sally", 20)
-    patron_2.add_interest("IMAX")
+    patron2 = Patron.new("Sally", 20)
+    patron2.add_interest("@imax")
 
-    assert_equal [dead_sea_scrolls, gems_and_minerals],dmns.recommend_exhibits(patron_1)
-    assert_equal [imax],dmns.recommend_exhibits(patron_2)
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals],@dmns.recommend_exhibits(patron1)
+    assert_equal [@imax],@dmns.recommend_exhibits(patron2)
   end
+
+  def test_it_can_show_patrons_by_exhibit_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [], @dmns.patrons
+
+    patron1 = Patron.new("Bob", 0)
+    patron1.add_interest("Gems and Minerals")
+    patron1.add_interest("Dead Sea Scrolls")
+
+    patron2 = Patron.new("Sally", 20)
+    patron2.add_interest("Dead Sea Scrolls")
+
+    patron3 = Patron.new("Johnny", 5)
+    patron3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(patron1)
+    @dmns.admit(patron2)
+    @dmns.admit(patron3)
+
+    assert_equal [patron1, patron2, patron3], @dmns.patrons
+
+    expected = {
+      @gems_and_minerals => [patron1],
+      @dead_sea_scrolls => [patron1, patron2, patron3],
+      @imax => []
+    }
+
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
+
+  def test_it_can_show_ticket_lottery_contestants
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [], @dmns.patrons
+
+    patron1 = Patron.new("Bob", 0)
+    patron1.add_interest("Gems and Minerals")
+    patron1.add_interest("Dead Sea Scrolls")
+
+    patron2 = Patron.new("Sally", 20)
+    patron2.add_interest("Dead Sea Scrolls")
+
+    patron3 = Patron.new("Johnny", 5)
+    patron3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(patron1)
+    @dmns.admit(patron2)
+    @dmns.admit(patron3)
+
+    assert_equal [], @dmns.ticket_lottery_contestants
+  end
+
+  def it_can_announce_lottery_winner
+    skip
+  end
+
 end
