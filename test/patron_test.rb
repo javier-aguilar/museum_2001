7b6f84694eda192bc7d6593fc50daacc17f0d353
Javier Aguilar <jvr.agx@gmail.com>
2020-02-19 09:20:03 -0700

    Pass test_it_has_attributes

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,7 +12,6 @@ class PatronTest < Minitest::Test
 
 
   def test_it_has_attributes
-    skip
     patron1 = Patron.new("Bob", 20)
     assert_equal "Bob", patron1.name
     assert_equal 20, patron1.spending_money
