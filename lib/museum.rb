81c5c47304ce7124a295e9006ef2e09daee1b000
Javier Aguilar <jvr.agx@gmail.com>
2020-02-19 09:44:34 -0700

    Pass test_it_can_recommend_exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,4 +11,14 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommend_exhibits(patron)
+    recommended = []
+    patron.interests.each do | interest |
+      @exhibits.each do | exhibit |
+        recommended << exhibit if interest == exhibit.name
+      end
+    end
+    recommended
+  end
+
 end
