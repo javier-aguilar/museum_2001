cbeeacfa1b8fdc4fccf76ddae6d7bd4675b6cdc3
Javier Aguilar <jvr.agx@gmail.com>
2020-02-19 09:13:52 -0700

    Fix assign variables

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -6,13 +6,13 @@ require './lib/patron'
 class ExhibitTest < Minitest::Test
 
   def test_it_exists
-    exhibit = exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     assert_instance_of Exhibit, exhibit
   end
 
 
   def test_it_has_attributes
-    exhibit = exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     assert_equal "Gems and Minerals", exhibit.name
     assert_equal 0, exhibit.cost
   end
