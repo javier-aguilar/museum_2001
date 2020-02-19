8f95032961c1ff2b1b49f41d23a613d5a6b92a20
Javier Aguilar <jvr.agx@gmail.com>
2020-02-19 09:12:44 -0700

    Pass test_it_has_attributes

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -12,6 +12,9 @@ class ExhibitTest < Minitest::Test
 
 
   def test_it_has_attributes
-
+    exhibit = exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
   end
+
 end
